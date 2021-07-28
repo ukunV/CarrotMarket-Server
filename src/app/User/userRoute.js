50246b38d506eedbc72d8615b0d47d7b7e60f6e3
ukunV <showklee@naver.com>
2021-07-29 02:30:17 +0900

    modify: getMyCarrot_path

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   app.post("/login", user.createUser);
 
   // 2. 나의 당근 조회 API
-  app.get("/user", jwtMiddleware, user.getMyCarrot);
+  app.get("/user/my-carrot", jwtMiddleware, user.getMyCarrot);
 
   // 3. 회원 프로필 조회 API
   app.get("/user/:selectId/profile", jwtMiddleware, user.getUserProfile);
